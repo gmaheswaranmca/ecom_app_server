ff9ece67796368f4eda0567c2237e2fe171a1db1
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-09-18 07:33:37 +0530

    ecom app server

--- src/main/java/com/mahesh/ecomsystem/ProductController.java
+++ src/main/java/com/mahesh/ecomsystem/ProductController.java
@@ -1,7 +1,6 @@
 package com.mahesh.ecomsystem;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,35 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProdController {
+@RequestMapping(path="/products")
+public class ProductController {
 	@Autowired
-	private ProductRepo repo;
-	
-	
-	
-	@PostMapping(path="products")
+	private ProductRepository repo;
+	@PostMapping
 	public Product create(@RequestBody Product product) {
 		product.setId(null);
 		Product savedProduct = repo.save(product);		
 		return savedProduct;
 	}
-	
-	@GetMapping(path="products")
+	@GetMapping
 	public List<Product> findAll() {
 		List<Product> products = repo.findAll();
 		return products;
 	}
-	
-	@GetMapping(path="products/{id}")
+	@GetMapping(path="/{id}")
 	public Product findById(@PathVariable String id) {
 		Product product = repo.findById(id).get();
 		return product;
 	}
-	
-	@PutMapping(path="products/{id}")
+	@PutMapping(path="/{id}")
 	public Product update(@PathVariable String id, @RequestBody Product product) {
 		Product oldProduct = repo.findById(id).get();
 		oldProduct.setName(product.getName());
@@ -52,8 +47,7 @@ public class ProdController {
 		Product updatedProduct = repo.save(oldProduct);
 		return updatedProduct;
 	}
-	
-	@DeleteMapping(path="products/{id}")
+	@DeleteMapping(path="{id}")
 	public boolean delete(@PathVariable String id) { 
 		Optional<Product> optionalProduct = repo.findById(id);
 		if(optionalProduct.isEmpty()) {
@@ -62,19 +56,4 @@ public class ProdController {
 		repo.deleteById(id);
 		return true;
 	}
-	
-	@Autowired
-    private TokenUtil jwtUtil;
-
-    // Fake login just to issue token
-    @PostMapping("/auth")
-    public Map<String, String> authenticate(@RequestBody String username) {
-        String token = jwtUtil.generateToken(username);
-        return Map.of("token", token);
-    }
-
-    @GetMapping("/api/products")
-    public List<String> getProducts() {
-        return List.of("Laptop", "Phone", "Tablet", "Headphones");
-    }
 }
--- src/main/java/com/mahesh/ecomsystem/ProductRepository.java
+++ src/main/java/com/mahesh/ecomsystem/ProductRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepo extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 	
 }
-
